cb1e2741dd1b009da8d17d014fe433950dab3bda
yuu2003-dev <yuuharada2003@gmail.com>
2025-06-01 10:03:00 +0900

    スケジュール管理アプリ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   root 'posts#index'
   resources :posts
+=======
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+>>>>>>> f84748e03b62e68202098e0475096e77df52afc0
 end
